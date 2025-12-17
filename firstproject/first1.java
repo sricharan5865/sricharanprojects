941a955d240cf2274559e77a101b758151d10e05
sricharan5865 <sricharan586511@gmail.com>
2025-12-17 21:07:38 +0530

    antigravity

--- firstproject/first1.java
+++ firstproject/first1.java
@@ -1,5 +1,6 @@
 package firstproject;
-//this is the comment in the github
+
+//this is the comment in the antigravity
 public class first1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
